cacdbda2ca4a03cf392b79d8be52e784a98c3ecd
Eric Gazoni <eric@adimian.com>
2019-01-16 15:36:34 +0100

    add CI setup

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup
 
-
 setup(
-    name="redis_log_handler",
-    version="0.1",
-    description="A log handler for the Python logging module, emitting all logs to specific Redis channels.",
-    packages=["redis_log_handler"],
-)
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True, )
+from setuptools import setup
