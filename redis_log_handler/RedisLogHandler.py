cfaa98392dc39fda7d9fa01e9e1712c7f4c16dc5
Bram Vereertbrugghen <bram@adimian.com>
2019-01-21 08:16:51 +0100

    Moved Redis connection init to Handler init

--- redis_log_handler/RedisLogHandler.py
+++ redis_log_handler/RedisLogHandler.py
@@ -4,14 +4,22 @@ import redis
 
 
 class RedisLogHandler(logging.StreamHandler):
-    def __init__(self, channel: str, host: str = None, port: int = None, password: str = None):
+    def __init__(
+            self, channel: str, host: str = None, port: int = None,
+            password: str = None
+    ):
         super().__init__()
 
         self.channel = channel
         self.host = host or 'localhost'
         self.port = port or 6379
         self.password = password or None
+        self.redis_client = self._initialize_redis_connection()
+
+    def _initialize_redis_connection(self):
+        return redis.Redis(
+            host=self.host, port=self.port, password=self.password
+        )
 
     def emit(self, message: logging.LogRecord):
-        redis_client = redis.Redis(host=self.host, port=self.port, password=self.password)
-        redis_client.publish(self.channel, str(message))
+        self.redis_client.publish(self.channel, str(message))
